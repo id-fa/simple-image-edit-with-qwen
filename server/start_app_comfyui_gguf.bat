214e55be07784fc6f7d6be74eb6c7796987afa68
id-fa <225554+id-fa@users.noreply.github.com>
2026-04-09 19:01:39 +0900

    minor

--- server/start_app_comfyui_gguf.bat
+++ server/start_app_comfyui_gguf.bat
@@ -6,7 +6,8 @@ rem Without --comfyui-path, LoRAs are discovered from ComfyUI automatically.
 
 python app_comfyui_gguf.py --host 0.0.0.0 --port 18188 --password password --steps 8 --gallery^
  --comfyui-url "http://127.0.0.1:8188"^
- --preset "画像1に画像2の服を着せる::将 Image-1 中的角色穿上 Image-2 的服装"^
+ --preset "画像2の服を着せる::将图像 1 中的角色穿上图像 2 中的服装。无视物理定律，穿一模一样的服装。不要添加额外的绳子或布料。"^
+ --preset "画像2のポーズにする::The character in image 1 is in the pose of image 2"^
  --preset "高画質化::Enhance quality."^
  --preset "テキスト除去::Remove all text."^
  --preset "LoRA_PAINT::Color this panelpainter"^
